8ea0354f3ed2a116437c3de3815aa1b51ccf9315
bruhboycarti <popkovs194@gmail.com>
2024-11-12 13:12:02 +0300

    gotovo

--- lab.3/lab.3.cpp
+++ lab.3/lab.3.cpp
@@ -1,104 +1,234 @@
-﻿#include <stdlib.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE  
+#define _CRT_NONSTDC_NO_DEPRECATE
+
 #include <stdio.h>
-#include <time.h>
-#include <Windows.h> 
-
-void shell(int* items, int count)
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+struct node
 {
+	char inf[256];  // полезная информация
+	int prior;
+	struct node* next; // ссылка на следующий элемент 
+};
+struct node* head = NULL, * last = NULL, * f = NULL; // указатели на первый и последний элементы списка
+char specchar = '+';
+int g = 0;
 
-    int i, j, gap, k;
-    int x, a[5];
+int spstore();
+void review(void), del(char* name);
+char find_el[256];
+struct node* find(char* name); // функция нахождения элемента
+struct node* get_struct(void); // функция создания элемента
+struct node* get_struct(void)
+{
+	struct node* p = NULL;
+	char s[256];
+	int num = 0;
 
-    a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
+	{
+		printf("Ошибка при распределении памяти\n");
+		exit(1);
 
-    for (k = 0; k < 5; k++) {
-        gap = a[k];
-        for (i = gap; i < count; ++i) {
-            x = items[i];
-            for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
-                items[j + gap] = items[j];
-            items[j + gap] = x;
-        }
-    }
+	}
+
+	printf("Введите название объекта: \n");   // вводим данные
+	scanf("%s", s);
+	fflush(stdin);
+	if (*s == 0)
+	{
+		printf("Запись не была произведена\n");
+		return NULL;
+	}
+	strcpy(p->inf, s);
+
+	printf("Введите приоритет объекта: \n");   // вводим данные
+	scanf("%d", &num);
+	fflush(stdin);
+
+	p->prior = num;
+
+	p->next = NULL;
+
+	return p;		// возвращаем указатель на созданный элемент
 }
 
-void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
+/* Последовательное добавление в список элемента (в конец)*/
+int spstore()
 {
-    int i, j;
-    int x, y;
+	struct node* p = NULL;
+	struct node* x = NULL;
+	struct node* n = NULL;
+	struct node* struc = head;
+	p = get_struct();
+	fflush(stdin);
+	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
+	{
+		head = p;
+		last = p;
+	}
+	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец 
+	{
+		while (struc)
+		{
+			n = struc->next;
+			if ((struc->prior < p->prior) && (n == NULL)) {
+				struc->next = p;
+				last = p;
+				break;
+			}
+			else if ((struc->prior > p->prior)) {
+				p->next = struc;
+				if (x != NULL)
+				{
+					x->next = p;
+				}
+				if (struc == head) {
+					head = p;
+				}
+				break;
+			}
+			else if ((struc->prior <= p->prior)) {
+				x = struc;
+				struc = struc->next;
+			}
+		}
+	}
+	printf("Продолжаем ввод? 1/0 \n");
+	scanf(" %c", &specchar);
+	getchar();
+	if (specchar == '1') {
 
+		g = 1;
+	}
+	else {
+		g = 0;
+	}
 
-
-    i = left; j = right;
-
-    /* выбор компаранда */
-    x = items[(left + right) / 2];
-
-    do {
-        while ((items[i] < x) && (i < right)) i++;
-        while ((x < items[j]) && (j > left)) j--;
-
-        if (i <= j) {
-            y = items[i];
-            items[i] = items[j];
-            items[j] = y;
-            i++; j--;
-        }
-    } while (i <= j);
-
-    if (left < j) qs(items, left, j);
-    if (i < right) qs(items, i, right);
+	return (g);
 }
-int compare(const void* x1, const void* x2)   /*функция сравнения элементов массива*/
+
+
+/* Просмотр содержимого списка. */
+void review(void)
 {
-    return (*(int*)x1 - *(int*)x2);              /* <0	elem1 меньше elem2,  =0	elem1 эквивалентен elem2,   >0	elem1 больше elem2*/
+	struct node* struc = head;
+	if (head == NULL)
+	{
+		printf("Список пуст\n");
+	}
+	while (struc)
+	{
+		printf("Имя - %s, Приоритет - %d \n", struc->inf, struc->prior);
+		struc = struc->next;
+	}
+	return;
 }
-
 int main() {
-    const int count = 100000;
-    double tim1, tim2, tim3;
-    clock_t start, end; // объявляем переменные для определения времени выполнени
-    /*int b[count], c[count], d[count];*/
-    int* g = (int*)malloc(count * sizeof(int));
-    int* b = (int*)malloc(count * sizeof(int));
-    int* c = (int*)malloc(count * sizeof(int));
-    for (int i = 0; i < count / 2; i++) {
-        b[i] = i;
-    }
-    for (int i = count / 2; i < count; i++) {
-        b[i] = b[49999] - 1;
-    }
-    for (int i = 0; i < count; i++) {
-        c[i] = b[i];
-    }
-    for (int i = 0; i < count; i++) {
-        g[i] = b[i];
-    }
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	spstore();
 
-    //SHELL
-    start = clock();
-    shell(b, count);
-    end = clock();
-    tim1 = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("shell");
-    printf("\n%lf", tim1);
-
-    //QS
-    start = clock();
-    qs(c, 0, count - 1);
-    end = clock();
-    tim2 = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("\n\nqs");
-    printf("\n%lf", tim2);
-
-    //QSORT
-    start = clock();
-    qsort(g, count, sizeof(int), compare);
-    end = clock();
-    tim3 = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("\n\nqsort");
-    printf("\n%lf", tim3);
-
-    return(0);
+	while (g == 1) {
+		g = spstore();
+	}
+	review();
 
 }
+
+
+
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_DEPRECATE  
+//#define _CRT_NONSTDC_NO_DEPRECATE
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <Windows.h>
+//struct node
+//{
+//	char inf[256];  // полезная информация
+//	struct node* next; // ссылка на следующий элемент 
+//};
+//struct node* head = NULL, * last = NULL, * f = NULL; // указатели на первый и последний элементы списка
+//char specchar;
+//int g = 0;
+//int spstore();
+//void review(void), del(char* name);
+//char find_el[256];
+//struct node* find(char* name); // функция нахождения элемента
+//struct node* get_struct(void); // функция создания элемента
+//struct node* get_struct(void)
+//{
+//	struct node* p = NULL;
+//	char s[256];
+//	int num = 0;
+//	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
+//	{
+//		printf("Ошибка при распределении памяти\n");
+//		exit(1);
+//	}
+//	printf("Введите название объекта: \n");   // вводим данные
+//	scanf("%s", s);
+//	if (*s == 0)
+//	{
+//		printf("Запись не была произведена\n");
+//		return NULL;
+//	}
+//	strcpy(p->inf, s);
+//	p->next = NULL;
+//	return p;		// возвращаем указатель на созданный элемент
+//}
+///* Последовательное добавление в список элемента (в конец)*/
+//int spstore()
+//{
+//	struct node* p = NULL;
+//	p = get_struct();
+//	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
+//	{
+//		head = p;
+//		last = p;
+//	}
+//	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
+//	{
+//		last->next = p;//голова не меняется, все в конец(FIFO)
+//		last = p;
+//	}
+//	printf("Продолжаем ввод? 1/0 \n");
+//	scanf_s(" %c", &specchar);
+//	getchar();
+//	if (specchar == '1') {
+//		g = 1;
+//	}
+//	else {
+//		g = 0;
+//	}
+//	return (g);
+//}
+///* Просмотр содержимого списка. */
+//void review(void)
+//{
+//	struct node* struc = head;
+//	if (head == NULL)
+//	{
+//		printf("Список пуст\n");
+//	}
+//	while (struc)
+//	{
+//		printf("Имя - %s \n", struc->inf);
+//		struc = struc->next;
+//	}
+//	return;
+//}
+//int main() {
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	spstore();
+//	while (g == 1) {
+//		g = spstore();
+//	}
+//	review();
+//}
